d68f0e8109bd774952c15d399669c35ac4c650b7
qhfka74 <shj04367@gmail.com>
2025-03-14 09:12:42 +0900

    file_mybatis

--- src/main/java/com/kedu/dao/FileDAO.java
+++ src/main/java/com/kedu/dao/FileDAO.java
@@ -2,6 +2,7 @@ package com.kedu.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,16 +12,16 @@ import com.kedu.dto.FileDTO;
 
 @Repository
 public class FileDAO {
-	
+
 	@Autowired
-	private JdbcTemplate jdbc;
+	private SqlSession mybatis;
 
 	public int insert(FileDTO dto) {
-		String sql = "insert into files values(files_seq.nextval, ?, ?, ?)";
-		return this.jdbc.update(sql, dto.getOriName(), dto.getSysName(), dto.getParent_seq());
+		 return mybatis.insert("File.insert", dto);
 	}
+
+	// parent_seq를 기준으로 파일 목록 조회
 	public List<FileDTO> findBySeq(String parent_seq) {
-		String sql = "select * from files where parent_seq = ?";
-		return jdbc.query(sql, new BeanPropertyRowMapper<FileDTO>(FileDTO.class),parent_seq);
+		return mybatis.selectList("File.findBySeq", parent_seq);
 	}
 }
