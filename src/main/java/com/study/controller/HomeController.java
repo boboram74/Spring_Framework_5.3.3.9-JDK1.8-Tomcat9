722c768943d913e9f4b74973984f5fd4b56b9237
qhfka74 <shj04367@gmail.com>
2025-07-30 13:30:12 +0900

    qhfka74/first

--- src/main/java/com/study/controller/HomeController.java
+++ src/main/java/com/study/controller/HomeController.java
@@ -1,9 +1,7 @@
-package com.kedu.controller;
+package com.study.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
--- src/main/java/utils/Statics.java
+++
@@ -1,7 +0,0 @@
-package utils;
-
-public class Statics {
-
-	public static int recordCountPerPage = 10;
-	public static int naaviCountPerPage = 10;
-}
